a03c87fe584113192827c41c906fb881367d4ecf
caiobrandaoo <202409055131@alunos.estacio.br>
2025-03-28 02:20:38 +0000

    simulei os movimentos da rainha , do bispo e da torre usando o for

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main()
+{
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -27,6 +28,55 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+    int torre = 1, bispo = 3, rainha = 2, escolha;
+    printf("escolha a peça que quer movimentar\n");
+    printf("1:torre\n");
+    printf("2:bispo\n");
+    printf("3:rainha\n");
+    scanf("%d", &escolha);
+    switch (escolha)
+    {
+    case 1:
+        escolha = torre;
+        break;
+    case 2:
+        escolha = bispo;
+        break;
+    case 3:
+        escolha = rainha;
+        break;
+    default:
+        printf("peça inexistente");
+        break;
+    };
+    if (escolha == torre)
+    {
+        for (escolha; escolha <= 5; escolha++)
+        {
+            printf("direita\n");
+        };
+    }
+    else if (escolha == bispo)
+    {
+        for (escolha; escolha <= 12; escolha++)
+        {
+            if (escolha % 2 != 0)
+            {
+                printf("cima\n");
+            }
+            else
+            {
+                printf("direita\n");
+            }
+        }
+    }
+    else if (escolha == rainha)
+    {
+        for (escolha; escolha <= 9; escolha++)
+        {
+            printf("esquerda\n");
+        }
+    }
 
     return 0;
 }
