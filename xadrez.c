9286e8b8feb8886b21bb8aaabbf714c4efd1ef93
caiobrandaoo <202409055131@alunos.estacio.br>
2025-03-29 04:36:56 +0000

    implementei funçoes de recursividade

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,32 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// implementar recursividade torre,bispo e rainha
+void movertorre(int n)
+{
+    if (n > 0)
+    {
+        printf("cima\n");
+        movertorre(n - 1);
+    }
+}
+void moverrainha(int n)
+{
+    if (n > 0)
+    {
+        printf("esquerda\n");
+        moverrainha(n - 1);
+    }
+}
+void moverbispo(int n)
+{
+    if (n > 0)
+    {
+        printf("cima\ndireita\n");
+        moverbispo(n - 1);
+    }
+}
+
 
 int main()
 {
@@ -29,154 +55,28 @@ int main()
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
     int torre, bispo, rainha, cavalo, escolha;
-    char direcao, direcao2;
     printf("escolha a peça que quer movimentar\n");
     printf("1:torre\n");
     printf("2:bispo\n");
     printf("3:rainha\n");
     printf("4: cavalo\n");
     scanf("%d", &escolha);
+    
     switch (escolha)
     {
     case 1:
-        escolha = torre;
+        movertorre(5);
         break;
     case 2:
-        escolha = bispo;
+        moverbispo(5);
         break;
     case 3:
-        escolha = rainha;
+        moverrainha(8);
         break;
     case 4:
-        // Movimentação do Cavalo
-        printf("Escolha a direção do cavalo:\n");
-        printf("1: Cima\n");
-        printf("2: Baixo\n");
-        printf("3: Direita\n");
-        printf("4: Esquerda\n");
-        getchar();             // Para limpar o buffer do Enter anterior
-        scanf("%c", &direcao); // Direção principal do cavalo
-
-        printf("Escolha a segunda direção do cavalo:\n");
-        printf("1: Direita\n");
-        printf("2: Esquerda\n");
-        getchar();              // Para limpar o buffer do Enter anterior
-        scanf("%c", &direcao2); // Segunda direção do cavalo
-
-        printf("Movimentos do cavalo:\n");
-
-        // Movimentação do cavalo com base nas direções
-        if (direcao == '1' && direcao2 == '1')
-        {
-            // 2 casas para cima e 1 para a direita
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para cima\n");
-            }
-            printf("Cavalos se movem: 1 casa para a direita\n");
-        }
-        else if (direcao == '1' && direcao2 == '2')
-        {
-            // 2 casas para cima e 1 para a esquerda
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para cima\n");
-            }
-            printf("Cavalos se movem: 1 casa para a esquerda\n");
-        }
-        else if (direcao == '2' && direcao2 == '1')
-        {
-            // 2 casas para baixo e 1 para a direita
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para baixo\n");
-            }
-            printf("Cavalos se movem: 1 casa para a direita\n");
-        }
-        else if (direcao == '2' && direcao2 == '2')
-        {
-            // 2 casas para baixo e 1 para a esquerda
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para baixo\n");
-            }
-            printf("Cavalos se movem: 1 casa para a esquerda\n");
-        }
-        else if (direcao == '3' && direcao2 == '1')
-        {
-            // 2 casas para a direita e 1 para cima
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para a direita\n");
-            }
-            printf("Cavalos se movem: 1 casa para cima\n");
-        }
-        else if (direcao == '3' && direcao2 == '2')
-        {
-            // 2 casas para a direita e 1 para baixo
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para a direita\n");
-            }
-            printf("Cavalos se movem: 1 casa para baixo\n");
-        }
-        else if (direcao == '4' && direcao2 == '1')
-        {
-            // 2 casas para a esquerda e 1 para cima
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para a esquerda\n");
-            }
-            printf("Cavalos se movem: 1 casa para cima\n");
-        }
-        else if (direcao == '4' && direcao2 == '2')
-        {
-            // 2 casas para a esquerda e 1 para baixo
-            for (int i = 1; i <= 2; i++)
-            {
-                printf("Cavalos se movem: 1 casa para a esquerda\n");
-            }
-            printf("Cavalos se movem: 1 casa para baixo\n");
-        }
-        else
-        {
-            printf("Direção inválida.\n");
-        }
+        for(int i=0;i<1;i++){for(int j=0;j<2;j++){printf("cima\n");}
+        printf("direita\n");}
         break;
-    default:
-        printf("Peça inexistente.\n");
-        break;
-
-   
-    }
-    if (escolha == torre)
-    {
-        for (int i = 1; i <= 5; i++)
-        {
-            printf("direita\n");
-        };
-    }
-    else if (escolha == bispo)
-    {
-        for (int i = 1; i <= 10; i++)
-        {
-            if (i % 2 != 0)
-            {
-                printf("cima\n");
-            }
-            else
-            {
-                printf("direita\n");
-            }
-        }
-    }
-    else if (escolha == rainha)
-    {
-        for (int i = 1; i <= 8; i++)
-        {
-            printf("esquerda\n");
-        }
-    }
-
-    return 0;
+        
+    }return 0;
 }
